2c92db5145d2e0c386034bf2463201eed8cfa7e6
Danila Nikonets <danila.nikonets@gmail.com>
2022-01-25 18:51:10 +0500

    temp commented android.support.annotation.Nullable

--- android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
+++ android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
@@ -4,7 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import android.app.Activity;
-import android.support.annotation.Nullable;
+// import android.support.annotation.Nullable;
 import android.util.Log;
 import android.util.SparseArray;
 
