f8ef2cb03f0cef98681aecb37c03a8195f2fee86
Danila Nikonets <danila.nikonets@gmail.com>
2022-08-15 00:35:08 +0500

    mock EventEmitter

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -2,6 +2,8 @@ import { NativeModules, NativeEventEmitter } from 'react-native';
 
 const { WebRTCModule } = NativeModules;
 
-const EventEmitter = new NativeEventEmitter(WebRTCModule);
+const EventEmitter = {
+  addListener: (string, func: (string) => any) => {}
+} //new NativeEventEmitter(WebRTCModule);
 
 export default EventEmitter;
